8f7208f475f4fa42f1ef074d4e31c5dd6d45edae
martcpp <mordecaiemmanueletukudo@gmail.com>
2025-03-24 19:01:19 +0100

    added lincense MIT

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, Responder,HttpResponse,HttpRequest};
+
+
+async fn index(req:HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+            .route("/{name}", web::get().to(index))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+   
 }
